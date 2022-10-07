7d57bc863399ea7a6331f480ea0e5984c3a43dbb
Derek <derek.mears@outlook.com>
2022-10-07 01:01:39 -0400

    refactored and db connection

--- ML_Analysis.py
+++ ML_Analysis.py
@@ -1,49 +1,61 @@
 import pandas as pd
 import os
 import plotly.express as px
+from Team1_Project.config import pwd, username, db_name, host, port
+import sqlalchemy
+from sqlalchemy import create_engine
+target = ['average_price']
 
-target = ['Average Price']
-df_all = pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','GTA_HomePrice_History.csv'),index_col=0)
+#df_all = pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','GTA_HomePrice_History.csv'),index_col=0)
+postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=username,password=pwd,ipaddress=host,port=port,dbname=db_name))
+conn = create_engine(postgres_str)
+df_all = pd.read_sql('Select * from home_prices',con=conn)
+#inflation = pd.read_sql('Select * from home_prices')
+recession = pd.read_sql('Select * from recession_indicator',con=conn)
+mortgage_rates = pd.read_sql('Select * from interest_rate',con=conn)
+inflation = pd.read_sql('Select * from inflation',con=conn)
 
-def frame_format(df_in,float_cols,date_cols):
-
-    df = df_in.copy()
-
-    for col in float_cols:
-        for s in ['$', ',', '%', ' ']:
-            df.loc[:, col] = df.loc[:,col].str.replace(s,'')
-
-        df.loc[:,col] = df.loc[:,col].astype('float')
-    for col in date_cols:
-        df.loc[:,col] = df.loc[:,col].apply(lambda x: pd.Period(x))
-    #add in year and quarter cols, drop original date column
-    if 'Date' in df.columns:
-        df['Year'] = df.Date.dt.year
-        df['Quarter'] = df.Date.dt.quarter
-    return df
-
-#cleaning raw data (type converstion and symbol removal from strings)
-df_all = frame_format(df_all,['Average Price','Average SP/LP'],['Date'])
-df_all.set_index('Date',inplace=True)
-#df_all.drop('Date',inplace=True, axis=1)
+# #data preprocessing no longer needed, database source has precleaned data
+# def frame_format(df_in,float_cols,date_cols):
+#
+#     df = df_in.copy()
+#
+#     for col in float_cols:
+#         for s in ['$', ',', '%', ' ']:
+#             df.loc[:, col] = df.loc[:,col].str.replace(s,'')
+#
+#         df.loc[:,col] = df.loc[:,col].astype('float')
+#     for col in date_cols:
+#         df.loc[:,col] = df.loc[:,col].apply(lambda x: pd.Period(x))
+#     #add in year and quarter cols, drop original date column
+#     if '_date' in df.columns:
+#         df['_year'] = df.Date.dt.year
+#         df['quarter'] = df.Date.dt.quarter
+#     return df
+#
+# #cleaning raw data (type converstion and symbol removal from strings)
+# df_all = frame_format(df_all,['average_price,'Average_SP_LP'],['_date'])
+df_all._date = df_all._date.apply(lambda x: pd.Period(x))
+df_all.set_index('_date',inplace=True)
+#df_all.drop('_date',inplace=True, axis=1)
 #filter dataset to toronto only, require avg price to >0, and take only community/date groups that have
 #more than 30 quarters of data available
-df_all_toronto = df_all.loc[df_all.Area=='Toronto',:]
-df_all_toronto = df_all_toronto.loc[df_all_toronto['Average Price']>0,:]
+df_all_toronto = df_all.loc[df_all.area=='Toronto',:]
+df_all_toronto = df_all_toronto.loc[df_all_toronto['average_price']>0,:]
 
 #df_all_toronto.notna()
 
 
 
-element_group_sizes = df_all_toronto.groupby(['Community','Building_Type']).size()>30
+element_group_sizes = df_all_toronto.groupby(['community','building_type']).size()>30
 element_group_sizes=element_group_sizes[element_group_sizes==1]
 #sized groups
 grps=tuple(zip(element_group_sizes.reset_index().iloc[:,0].to_list(),element_group_sizes.reset_index().iloc[:,1].to_list()))
-element_group_sizes = df_all_toronto.groupby(['Community','Building_Type']).size()>30
-df_all_toronto_clean=pd.concat([df_all_toronto.groupby(['Community','Building_Type']).get_group(x) for x in grps])
+element_group_sizes = df_all_toronto.groupby(['community','building_type']).size()>30
+df_all_toronto_clean=pd.concat([df_all_toronto.groupby(['community','building_type']).get_group(x) for x in grps])
 
-df_all_toronto_clean.sort_values(by=['Community','Year','Quarter'],inplace=True)
-df_all_toronto_clean.drop(['Area','Municipality', 'Dollar Volume'],inplace=True,axis=1)
+df_all_toronto_clean.sort_values(by=['community','building_type','_year','quarter'],inplace=True)
+df_all_toronto_clean.drop(['area','municipality', 'dollar_volume','_no'],inplace=True,axis=1)
 
 
 ############Autocorrelation check##################################
@@ -53,9 +65,9 @@ df_all_toronto_clean.drop(['Area','Municipality', 'Dollar Volume'],inplace=True,
 # # from the statsmodel library passing
 # # our DataFrame object in as the data
 # # Note: Limiting Lags to 50
-# acfdata = df_all_toronto_clean[(df_all_toronto_clean.Community=='Junction Area')&(df_all_toronto_clean.Building_Type=='Detached')][['Year','Quarter','Average Price']]
+# acfdata = df_all_toronto_clean[(df_all_toronto_clean.Community=='Junction Area')&(df_all_toronto_clean.Building_Type=='Detached')][['_year','quarter','average_price]]
 # acfdata.Date=acfdata.Date.apply(lambda x: pd.Period(x).end_time.date())
-# acfdata.set_index(['Date'],inplace=True)
+# acfdata.set_index(['_date'],inplace=True)
 # plot_acf(x=acfdata, lags=25)
 # # Show the AR as a plot
 # plt.show()
@@ -66,43 +78,53 @@ df_all_toronto_clean.drop(['Area','Municipality', 'Dollar Volume'],inplace=True,
 
 
 
-
-inflation=pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','BoCInflation.csv'),header=12,index_col=0)
-inflation.index = pd.to_datetime(inflation.index)
-inflation_q=inflation.resample('q').agg('mean')
-inflation_q.index=inflation_q.index.to_period('q')
-
-mortgage_rates=pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','BankofCanada-5yearMortgageRates.csv'),index_col=0)
-mortgage_rates.index = pd.to_datetime(mortgage_rates.index)
-mortgage_rates_q = mortgage_rates.resample('q').agg('mean')
-mortgage_rates_q.index=mortgage_rates_q.index.to_period('q')
-
-recession = pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','CanadaRecessionIndicator.csv'),header=0,index_col=0,dtype={'CANRECDM':float})
-recession_q = recession.copy()
-recession_q.index=pd.to_datetime(recession_q.index)
+#
+# inflation=pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','OffGitHub','MLFiles','BoCInflation.csv'),header=12,index_col=0)
+# inflation.index = pd.to_datetime(inflation.index)
+# inflation_q=inflation.resample('q').agg('mean')
+# inflation_q.index=inflation_q.index.to_period('q')
+#
+# mortgage_rates=pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','BankofCanada-5yearMortgageRates.csv'),index_col=0)
+# mortgage_rates.index = pd.to_datetime(mortgage_rates.index)
+# mortgage_rates_q = mortgage_rates.resample('q').agg('mean')
+# mortgage_rates_q.index=mortgage_rates_q.index.to_period('q')
+#
+# recession = pd.read_csv(os.path.join(os.getcwd(),'Team1_Project','Resources','CanadaRecessionIndicator.csv'),header=0,index_col=0,dtype={'CANRECDM':float})
+# recession_q = recession.copy()
+# recession_q.index=pd.to_datetime(recession_q.index)
 
 ###NEED TO ENSURE SERIES IS FLOAT TYPE - initial import had '.' in 2022Q3 row
 
-recession_q.index = recession_q.index.to_period('q')
+# recession_q.index = recession_q.index.to_period('q')
 
-df_all_toronto_clean_with_rates=df_all_toronto_clean.join(mortgage_rates_q,how='inner')
-df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.join(inflation_q['CPI_TRIM'],how='inner')
-df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.join(recession_q['CANRECDM'],how='inner')
+#df_all_toronto_clean_with_rates=df_all_toronto_clean.join(mortgage_rates_q,how='inner')
+idx = df_all_toronto_clean.index
+df_all_toronto_clean_with_rates=df_all_toronto_clean.\
+    merge(mortgage_rates,right_on='year_quarter_key',left_on='year_quarter_key',how='left')
+#df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.join(inflation_q['CPI_TRIM'],how='inner')
+
+df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.\
+    merge(inflation,right_on='year_quarter_key',left_on='year_quarter_key',how='left')
+#df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.join(recession_q['CANRECDM'],how='inner')
+df_all_toronto_clean_with_rates=df_all_toronto_clean_with_rates.\
+    merge(recession[['year_quarter_key','canrecdm']],right_on='year_quarter_key',left_on='year_quarter_key',how='left')
+
+df_all_toronto_clean_with_rates.set_index(idx,inplace=True)
 
 from sktime.forecasting.naive import NaiveForecaster
 import sktime
 from sktime.forecasting.base import ForecastingHorizon
 from sktime.performance_metrics.forecasting import MeanAbsolutePercentageError
 
-X = df_all_toronto_clean_with_rates.reset_index().set_index(['Community','Building_Type','index'])[['Average Price','V121764','CPI_TRIM','CANRECDM']]
+X = df_all_toronto_clean_with_rates.reset_index().set_index(['community','building_type','_date'])[['average_price','avg_five_year_rates','CPI_TRIM','canrecdm']]
 X.sort_index(inplace=True)
-y=X['Average Price'].to_frame()
+y = X['average_price'].to_frame()
 
 cutoff='2018Q4'
-y_test=y.loc[(y.index.get_level_values('index')>cutoff)]
-X_test=y.loc[(X.index.get_level_values('index')>cutoff)]
-X_train=y.loc[(X.index.get_level_values('index')<=cutoff)]
-y_train=y.loc[(y.index.get_level_values('index')<=cutoff)]
+y_test=y.loc[(y.index.get_level_values('_date')>cutoff)]
+X_test=y.loc[(X.index.get_level_values('_date')>cutoff)]
+X_train=y.loc[(X.index.get_level_values('_date')<=cutoff)]
+y_train=y.loc[(y.index.get_level_values('_date')<=cutoff)]
 fh = ForecastingHorizon(y_test.index.get_level_values(level=2).unique(), is_relative=False)
 
 forecaster = NaiveForecaster(strategy="last", sp=4)
@@ -122,27 +144,27 @@ mape(y_test,y_pred2)
 import plotly.graph_objects as go
 def make_plots(y_train,y_pred2,y_test,community_filter,building_filter):
 
-    y_train_plot =y_train.loc[(y_train.index.get_level_values('Community')==community_filter)&(y_train.index.get_level_values('Building_Type')==building_filter)]
-    y_pred2_plot =y_pred2.loc[(y_pred2.index.get_level_values('Community')==community_filter)&(y_pred2.index.get_level_values('Building_Type')==building_filter)]
-    y_test_plot =y_test.loc[(y_test.index.get_level_values('Community')==community_filter)&(y_test.index.get_level_values('Building_Type')==building_filter)]
+    y_train_plot =y_train.loc[(y_train.index.get_level_values('community')==community_filter)&(y_train.index.get_level_values('building_type')==building_filter)]
+    y_pred2_plot =y_pred2.loc[(y_pred2.index.get_level_values('community')==community_filter)&(y_pred2.index.get_level_values('building_type')==building_filter)]
+    y_test_plot =y_test.loc[(y_test.index.get_level_values('community')==community_filter)&(y_test.index.get_level_values('building_type')==building_filter)]
 
     y_train_plot=y_train_plot.reset_index()
-    y_train_plot=y_train_plot.set_index('index')
-    y_train_plot=y_train_plot.drop(['Community','Building_Type'],axis=1)
+    y_train_plot=y_train_plot.set_index('_date')
+    y_train_plot=y_train_plot.drop(['community','building_type'],axis=1)
 
     y_pred2_plot=y_pred2_plot.reset_index()
-    y_pred2_plot=y_pred2_plot.set_index('index')
-    y_pred2_plot=y_pred2_plot.drop(['Community','Building_Type'],axis=1)
+    y_pred2_plot=y_pred2_plot.set_index('_date')
+    y_pred2_plot=y_pred2_plot.drop(['community','building_type'],axis=1)
 
     y_test_plot=y_test_plot.reset_index()
-    y_test_plot=y_test_plot.set_index('index')
-    y_test_plot=y_test_plot.drop(['Community','Building_Type'],axis=1)
+    y_test_plot=y_test_plot.set_index('_date')
+    y_test_plot=y_test_plot.drop(['community','building_type'],axis=1)
 
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=y_train_plot.index.to_timestamp(),y=y_train_plot['Average Price']))
-    fig.add_trace(go.Scatter(x=y_test_plot.index.to_timestamp(),y=y_test_plot['Average Price'],mode='lines'))
-    fig.add_trace(go.Scatter(x=y_pred2_plot.index.to_timestamp(),y=y_pred2_plot['Average Price'],mode='lines'))
+    fig.add_trace(go.Scatter(x=y_train_plot.index.to_timestamp(),y=y_train_plot['average_price']))
+    fig.add_trace(go.Scatter(x=y_test_plot.index.to_timestamp(),y=y_test_plot['average_price'],mode='lines'))
+    fig.add_trace(go.Scatter(x=y_pred2_plot.index.to_timestamp(),y=y_pred2_plot['average_price'],mode='lines'))
     fig.show(renderer='browser')
 
 make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
@@ -153,7 +175,7 @@ make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
 # # def prophet_prep(df):
 # #     df.index.name = 'ds'
 # #     df.reset_index(inplace=True)
-# #     df.rename(columns={'Average Price': 'y'}, inplace=True)
+# #     df.rename(columns={'average_price': 'y'}, inplace=True)
 # #     cols = [x for x in df.columns.to_list() if x not in ['y','ds']]
 # #     cols2 = ['ds','y',*cols]
 # #
@@ -197,7 +219,7 @@ make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
 # cols = df_all_toronto_clean_with_rates_dummies.columns.to_list()
 # cols2 = [cols[0],cols[2],cols[1],*cols[3:]]
 # df_all_toronto_clean_with_rates_dummies = df_all_toronto_clean_with_rates_dummies[cols2]
-# df_all_toronto_clean_with_rates_dummies.rename(columns={'Average Price':'y'},inplace=True)
+# df_all_toronto_clean_with_rates_dummies.rename(columns={'average_price':'y'},inplace=True)
 # df_all_toronto_clean_with_rates_dummies['ds']=df_all_toronto_clean_with_rates_dummies['ds'].apply(lambda x: x.end_time)
 #
 #
@@ -205,18 +227,18 @@ make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
 #
 #
 # junction_detached = df_all_toronto_clean_with_rates[(df_all_toronto_clean_with_rates.Community=='Junction Area')&(df_all_toronto_clean_with_rates.Building_Type=='Detached')]
-# y=junction_detached['Average Price']
-# #junction_detached.drop('Average Price',inplace=True,axis=1)
+# y=junction_detached['average_price']
+# #junction_detached.drop('average_price',inplace=True,axis=1)
 # jd2=junction_detached.shift(1)
 #
 # jd3=jd2.join(y,how='inner')
-# jd3['AvgPx_lag1']=jd3['Average Price'].shift(1)
-# jd3['AvgPx_lag2']=jd3['Average Price'].shift(2)
-# jd3['AvgPx_lag3']=jd3['Average Price'].shift(3)
-# jd3['AvgPx_lag4']=jd3['Average Price'].shift(4)
-# jd3['AvgPx_lag5']=jd3['Average Price'].shift(5)
+# jd3['AvgPx_lag1']=jd3['average_price'].shift(1)
+# jd3['AvgPx_lag2']=jd3['average_price'].shift(2)
+# jd3['AvgPx_lag3']=jd3['average_price'].shift(3)
+# jd3['AvgPx_lag4']=jd3['average_price'].shift(4)
+# jd3['AvgPx_lag5']=jd3['average_price'].shift(5)
 #
-# jd3.drop('Average Price',inplace=True,axis=1)
+# jd3.drop('average_price',inplace=True,axis=1)
 #
 # X_test=jd3['2016Q4':jd3.index.max()]
 # X_train=jd3[jd3.index.min():'2016Q3']
@@ -233,18 +255,18 @@ make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
 # fig = px.scatter(x=y_pred_df,y=y_test)
 # ally=y_pred_df.join(y_test,how='inner')
 # ally.columns
-# fig = px.scatter(ally, x=0,y='Average Price')
+# fig = px.scatter(ally, x=0,y='average_price')
 #
 #
-# #df_encoded = pd.get_dummies(df_all_toronto_clean.drop(columns=['Average Price']))
+# #df_encoded = pd.get_dummies(df_all_toronto_clean.drop(columns=['average_price']))
 #
 # from sklearn_pandas import DataFrameMapper as dfm
 #
 #
 # numerical_cols_to_scale=['Sales', 'Dollar Volume',
 #       'New Listings', 'Average SP/LP', 'Average DOM']
-# cat_cols_to_encode=['Building_Type','Community']
-# num_col_no_transform=['Year','Quarter']
+# cat_cols_to_encode=['building_type','community']
+# num_col_no_transform=['_year','quarter']
 #
 # mapper_inputs = []
 #
@@ -289,7 +311,7 @@ make_plots(y_train,y_pred2,y_test,'Junction Area','Semi-detached')
 #
 # df_all
 #
-# df_all_to = df_all_toronto.loc[df_all_toronto['Average Price']>0,:]
+# df_all_to = df_all_toronto.loc[df_all_toronto['average_price']>0,:]
 #
 #
 #
